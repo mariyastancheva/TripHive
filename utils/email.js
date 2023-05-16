3fa18e3a7bda94f418fe9fc8d6d2e72c98474324
Mariya Stancheva <mariya.stancheva@accedia.com>
2023-05-16 22:35:34 +0300

    change the mail

--- utils/email.js
+++ utils/email.js
@@ -9,7 +9,7 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Mariya Stancheva <mariya.stancheva@abv.bg>`;
+    this.from = `Mariya Stancheva <mariya.stancheva@triphive.io>`;
   }
 
   newTransport() {
